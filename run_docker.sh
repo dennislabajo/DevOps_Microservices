18a159e020e23ca089b9ac745e87bef5eed7d76d
dennislabajo <dlabajo@gmail.com>
2020-06-10 14:57:03 -0500

    feat: Completed tasks 5 through 7

--- run_docker.sh
+++ run_docker.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-VERSION=beta
+VERSION=v.2
 NAME=devops_microservices
+ID=dlabajo
 PORT=8080
+dockerpath=$ID/$NAME
+
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag $NAME:$VERSION .
+docker build --tag $dockerpath:$VERSION .
 # Step 2: 
 # List docker images
 docker image ls
 # Step 3: 
 # Run flask app
-docker run --name $NAME -p $PORT:80 --rm $NAME:$VERSION
+docker run --name $NAME -p $PORT:80 --rm $dockerpath:$VERSION
+
