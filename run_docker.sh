ca15c6ee9976c205dbb55742c0416eaceb8c90c2
dennislabajo <dlabajo@gmail.com>
2020-06-09 19:25:36 -0500

    feat: tasks 1-4, 8

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-
+VERSION=beta
+NAME=devops_microservices
+PORT=8080
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag $NAME:$VERSION .
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run --name $NAME -p $PORT:80 --rm $NAME:$VERSION
--- upload_docker.sh
+++ upload_docker.sh
@@ -2,14 +2,19 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
+NAME=devops_microservices
+VERSION=.1
 
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=dlabajo/$NAME
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $NAME:beta "$dockerpath:v$VERSION"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
